dbdb1f6a156312d198082b29b433aee055d27730
Abinaselva <134791257+Abinaselva@users.noreply.github.com>
2023-05-28 16:21:33 +0530

    this is my first project

---
+++ Javaexamples/src/Concepts/Myfirst_program.java
@@ -0,0 +1,10 @@
+package Concepts;
+
+public class Myfirst_program {
+
+	public static void main(String[] args) {
+		System.out.print("This is my first program");
+
+	}
+
+}
---
+++ Javaexamples/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author abina
+ *
+ */
+module Javaexamples {
+}
